104f8ef46dcfbba8765f5d158dd96aa7c57fcf9b
PRAJAY FAL DESAI <prajayfaldesai987@gmail.com>
2025-11-12 15:36:06 +0530

    added backend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,5 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+import mongoose from "mongoose";
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
-});
-
-const promisePool = pool.promise();
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('❌ Database connection failed:', err.message);
-    return;
-  }
-  console.log('✅ MySQL Database connected successfully');
-  connection.release();
-});
-
-module.exports = promisePool;
+export const connectDB= async ()=>{
+    await mongoose.connect('mongodb+srv://prajayfaldesai987_db_user:sw1Q2wdIw5ElcQvP@safhon.lv9v368.mongodb.net/INSTITUTE-ERP').then(()=>console.log("DB Connect"));
+}
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-require('./config/db');
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
 
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app config
+const app = express()
+const port =4000
 
-app.get('/', (req, res) => {
-  res.send( 'Welcome to the Institute ERP Backend API');
-});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+//db connect
+connectDB(); 
+
+app.get("/",(req, res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
+//mongodb+srv://prajayfaldesai987_db_user:sw1Q2wdIw5ElcQvP@safhon.lv9v368.mongodb.net/?appName=SAFHON
+
+
