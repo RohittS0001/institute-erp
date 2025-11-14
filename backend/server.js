05eb778c2269929b69d02040eaf1af0af130b3ed
PRAJAY FAL DESAI <prajayfaldesai987@gmail.com>
2025-11-14 23:00:35 +0530

    Your message about server.js changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import adminRoutes from './routes/adminRoutes.js';
-import admissionsRoutes from "./routes/admissions.js";
-import researchRoutes from "./routes/research.js";
+import adminRoutes from './routes/user/adminRoutes.js';
+
 
 // app config
 const app = express();
@@ -18,8 +17,7 @@ connectDB();
 
 // routes
 app.use("/api/admin", adminRoutes);
-app.use("/api/admissions", admissionsRoutes);
-app.use("/api/research", researchRoutes);
+
 
 // base route
 app.get("/", (req, res) => {
@@ -35,35 +33,3 @@ app.listen(port, () => {
 
 
 
-// import express from "express";
-// import cors from "cors";
-// import { connectDB } from "./config/db.js";
-// import adminRoutes from './routes/adminRoutes.js';
-// import admissionsRoutes from "./routes/admissions.js";
-// import researchRoutes from "./routes/research.js";
-
-// // app config
-// const app = express();
-// const port = 4000;
-
-// // middleware
-// app.use(express.json());
-// app.use(cors());
-
-// // db connect
-// connectDB();
-
-// // routes
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/admissions", admissionsRoutes);
-// app.use("/api/research", researchRoutes);
-
-// // base route
-// app.get("/", (req, res) => {
-//     res.send("API WORKING");
-// });
-
-// // start server
-// app.listen(port, () => {
-//     console.log(`Server Started on http://localhost:${port}`);
-// });
