dc55d6eaa1b3bd1dcfa6ff5e7e327d84eb96e294
Rohit Sasane <rohittsasane@gmail.com>
2025-12-14 22:36:25 +0530

    api connection with frontend

--- frontend/src/Service/api.js
+++ frontend/src/Service/api.js
@@ -1,4 +1,4 @@
-const API_URL = "backenderp-production-fe2b.up.railway.app"; // your backend port
+const API_URL = "https://backenderp-production-fe2b.up.railway.app"; // your backend port
 
 // Example API call to test backend connection
 export async function getMessage() {
